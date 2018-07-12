70d79c3117238767f85a7b9c85ebf9c66532db81
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-07-12 12:02:31 +0900

    Workaround on Python side

--- str_vec_hdf5/string_vec.py
+++ str_vec_hdf5/string_vec.py
@@ -1,5 +1,11 @@
 import h5py
+import numpy
 
 with h5py.File('tmp.h5', 'w') as f:
-    f['/str_vec'] = ['A', 'AB']
+    data = ['A', 'B']
+
+    # Python2 & 3
+    # Write ASCII variable-length strings
+    f.create_dataset('/str_vec', dtype=h5py.special_dtype(vlen=bytes), data=[numpy.string_(i) for i in data])
+
     f['/str'] = 'A'
